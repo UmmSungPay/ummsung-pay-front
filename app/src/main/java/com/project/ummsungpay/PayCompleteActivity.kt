a6e1173b5059cdfcb310c0e8d91ac7de0f404e29
Song Hyejin <2010382@sookmyung.ac.kr>
2023-05-13 14:42:10 +0900

    230509 [스플래시->...->핀설정완료]

---
+++ app/src/main/java/com/project/ummsungpay/PayCompleteActivity.kt
@@ -0,0 +1,11 @@
+package com.project.ummsungpay
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class ActivityPayComplete : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pay_complete)
+    }
+}
---
+++ app/src/main/java/com/project/ummsungpay/PinCompleteActivity.kt
@@ -0,0 +1,11 @@
+package com.project.ummsungpay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PinCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pin_complete)
+    }
+}
