7ac0da3cfd88ff53213eb563a3e1cc7118dcf183
Song Hyejin <2010382@sookmyung.ac.kr>
2023-05-23 17:19:42 +0900

    [ADD] 카메라로 텍스트 인식

---
+++ app/src/main/java/com/project/ummsungpay/ScopedExecutor.java
@@ -0,0 +1,35 @@
+package com.project.ummsungpay;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ScopedExecutor implements Executor {
+
+    private final Executor executor;
+    private final AtomicBoolean shutdown = new AtomicBoolean();
+
+    public ScopedExecutor(@NonNull Executor executor) {
+        this.executor = executor;
+    }
+
+    @Override
+    public void execute(@NonNull Runnable command) {
+        if (shutdown.get()) {
+            return;
+        }
+        executor.execute(
+                () -> {
+                    if (shutdown.get()) {
+                        return;
+                    }
+                    command.run();
+                });
+    }
+
+    public void shutdown() {
+        shutdown.set(true);
+    }
+}
