4c435725f0125f121a9854c56b038bb48846d63d
Song Hyejin <2010382@sookmyung.ac.kr>
2023-05-26 13:25:06 +0900

    [ADD] 카메라로 카드번호와 유효기간 인식

--- app/src/main/java/com/project/ummsungpay/CardInfoActivity.kt
+++ app/src/main/java/com/project/ummsungpay/CardInfoActivity.kt
@@ -4,11 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_card_info.button_confirm
+import kotlinx.android.synthetic.main.activity_card_info.numValue
+import kotlinx.android.synthetic.main.activity_card_info.validityValue
 
 class CardInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_info)
+
+        val txt = intent.getStringExtra("recognized text").toString()
+
+        val regexCardnum = Regex("""\d{4}\s\d{4}\s\d{4}\s\d{4}""")
+        val regexValidity = Regex("""\d{2}/\d{2}""")
+        //val regexCvc
+
+        val matchCardnum: MatchResult? = regexCardnum.find(txt)
+        val matchValidity: MatchResult?  = regexValidity.find(txt)
+        //val matchCvc: MatchResult? = regexCvc.find(txt)
+
+        val valueCardnum: String = matchCardnum!!.value
+        val valueValidity: String = matchValidity!!.value
+        //val valueCvc: String = matchCvc!!.value
+
+        numValue.text = valueCardnum
+        validityValue.text = valueValidity
+        //cvcValue.text = valueCvc
+
 
         button_confirm.setOnClickListener{
             val intent = Intent(this, CardAddCompleteActivity::class.java)
--- app/src/main/java/com/project/ummsungpay/CardaddFragment.kt
+++ app/src/main/java/com/project/ummsungpay/CardaddFragment.kt
@@ -1,12 +1,15 @@
 package com.project.ummsungpay
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 
 class CardaddFragment : Fragment() {
 
@@ -19,7 +22,7 @@ class CardaddFragment : Fragment() {
 
         button_add.setOnClickListener(object :View.OnClickListener {
             override fun onClick(v: View?) {
-                val intent = Intent(context, CardScanActivity::class.java)
+                val intent = Intent(context, CardRecognitionActivity::class.java)
                 startActivity(intent)
             }
         })
